3cd24a5f50cdca7631f4ec7acc273663118334d2
Mark Manley <mark.manley3@icloud.com>
2024-12-20 07:49:26 -0500

    add JsDoc @Examples

--- src/sanity/equal.ts
+++ src/sanity/equal.ts
@@ -1,5 +1,6 @@
 /**
  * ? Type Alias for Equal
+ * * @example Equal<t, true>
  */
 export type Equals<A, B> = Equal<A, B>;
 
@@ -7,6 +8,7 @@ export type Equals<A, B> = Equal<A, B>;
  * ? Expect types X and Y to be equal.
  * ! Equal<X, A & B> does not work if, e.g.,
  * ! X equals Y, but Y is A & B,
+ * * @example Equals<true, t>
  */
 export type Equal<X, Y> = (<T>() => T extends X ? 1 : 0) extends <
   T,
--- src/sanity/expect.ts
+++ src/sanity/expect.ts
@@ -13,16 +13,16 @@ export type f = false;
 /**
  * ? Type Alias for Expect
  * ! Semantically useful when pairing with Equal and true:
- * * type t = true;
- * * ExpectTo<Equal<true, t>>
+ * * @example ExpectTo<Equal<true, t>>
  */
 export type ExpectTo<T> = Expect<T>;
 
 /**
  * ? Expect<T>: T should be true
  * ! Semantically useful when paring with Equal and 2 types:
- * * type a = true;
- * * type b = true
+ * * @example
+ * * type a = t;
+ * * type b = t;
  * * Expect<Equals<a, b>
  */
 export type Expect<T> = T extends true ? true : never;
@@ -30,12 +30,13 @@ export type Expect<T> = T extends true ? true : never;
 /**
  * ? ExpectToBeTrue<T>: T should be true
  * ! shortcut to check if T is true
- * * ExpectToBeTrue<t>
+ * * @example ExpectToBeTrue<t>
  */
 export type ExpectToBeTrue<T> = T extends true ? true : never;
 
 /**
  * ? ExpectToBeTrue<T>: T should be true
  * ! shortcut to check if T is false
+ * * @example ExpectToBeFalse<f>
  */
 export type ExpectToBeFalse<T> = T extends false ? true : never;
