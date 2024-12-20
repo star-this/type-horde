68e08209a8a9e29b8ce6e5aa2255056b9a459bc5
Mark Manley <mark.manley3@icloud.com>
2024-12-20 08:33:31 -0500

    update @/src/logic/true.ts

--- src/logic/true.ts
+++ src/logic/true.ts
@@ -1,6 +1,10 @@
 /**
- * ? Utility Type to construct True types
+ * ? Utility Type True<T> to construct Truth types
  * @example True<true> is true
+ * * Can be used without a Type Parameter
+ * * as a shorthad for true
  * @example True is true
  */
-export type True<TruthValue = true> = TruthValue extends true ? true : false;
+export type True<TruthStatement = true> = TruthStatement extends true
+  ? true
+  : false;
