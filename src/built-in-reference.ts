b7e22256b49345380dbc38dbc096c2aab827baa8
Mark Manley <mark.manley3@icloud.com>
2024-12-20 06:53:21 -0500

    create comment table of built-in Utility Types w/ descriptions & examples

---
+++ src/built-in-reference.ts
@@ -0,0 +1,37 @@
+/**
+ * Given:
+ * type User = { id: string; name: string; age: number}
+ * class Error {...}
+ * interface I { method(this: ThisType<I>, ...params: any[])}
+ * function effectThunk (): void
+ * function logMsg(this: {timestamp: string}, msg: string): void
+ */
+/**
+ * NAME                   USED ON         ACTION                EXAMPLE
+ * ! Exclude                Unions          Excludes members      Exclude<'a' | 1, string> -> 1
+ * * Extract                Unions          Extracts members      Extract<'a' | 1, string> -> 'a'
+ * ! Omit                   Objects         Excludes properties   Omit<User, 'id'> -> {name: string; age: number}
+ * * Pick                   Objects         Extracts properties   Pick<UserObj, 'id'> -> {id: string}
+ * ! Partial                Objects         Make props optional   Partial<User> -> {id?: string; name?: string; age?: number}
+ * * Required               Objects         Make props required   Required<Partial<User>>> -> User
+ * * Readonly               Objects         Make props readonly   Readyonly<User> -> {readonly id: string; readonly name: string; readonly age: number}
+ * ? Record                 Unions,Types    Make KV object type   Record<'ceo'|'cog', User> -> {ceo: User, cog: User}
+ * ! NonNullable            Unions          Omit null | undefined NonNullable<Type | undefined> -> Type
+ * ? Awaited                Promises        infer unwrapped types Awaited<number, Promise<string> -> number | string
+ * ? ReturnType             Functions       infer fn return type  ReturnType<typeof effectThunk> -> void
+ * ? Parameters             Functions       infer fn parameters   Parameters<typeof effectThunk> -> []
+ * ? InstanceType           Classes         infer obj instanceof  InstanceType<typeof Error> -> <(error instanceof Error === true)>
+ * ? ThisType               Functions/Objs? infer this of object  I::method(this: ThisType<I>, params... any[]) -> this<(I)>
+ *      [example](https://www.geeksforgeeks.org/typescript-thistypetype-utility-type/)
+ * ? ConstructorParameters  Classes         infer new(...params)  ConstructorParameters<typeof Error> -> [message: string, options?: {context: any}]
+ *      [example](https://www.geeksforgeeks.org/typescript-constructorparameters-utility-type/)
+ * * ThisParameterType      Functions/Objs? infer this context    ThisParameterType<typeof logMsg> -> this<({timestamp: string})>
+ *      [example](https://www.geeksforgeeks.org/typescript-thisparametertypetype-utility-type/)
+ * ! OmitThisParameter      Functions/Objs? make tuple w/o this   OmitThisParameter<typeof logMsg> -> (msg: string) => void
+ *      [example](https://www.geeksforgeeks.org/typescript-omitthisparameter-utility-type/)
+ * ! Lowercase              strings         make all chars lower  Lowercase<typeof ("HelloWorld" | "BOO")> -> "helloworld" | "boo"
+ * * Uppercase              strings         make all chars upper  Uppercase<typeof ("HelloWorld" | "boo")> -> "HELLOWORLD" | "BOO"
+ * ! Uncapitalize           strings         make first char lower Uncapitalize<typeof ("HelloWord" | "BOO")> -> "helloWorld" | "bOO"
+ * * Capitalize             strings         make first char upper Capitalize<typeof ("helloWorld" | "boo")> -> "HelloWorld" | "Boo"...
+ * * ...
+ */
