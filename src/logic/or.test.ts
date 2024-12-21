3424cf2e6ee51353726ba00fe9aac8bdd1f31484
Mark Manley <mark.manley3@icloud.com>
2024-12-20 21:25:23 -0500

    create Or w/ Truth Table tests

---
+++ src/logic/or.test.ts
@@ -0,0 +1,31 @@
+import { expect, suite, test } from "vitest";
+import { ExpectTo } from "../sanity/expect";
+import { Equal } from "../sanity/equal";
+import { Suite } from "../sanity/suite";
+import { Test } from "../sanity/test";
+import { t } from "../sanity/true";
+import { Or } from "./or";
+
+suite("Or<A, B>", () => {
+  type OrPasses = Suite<
+    "Or<A, B>",
+    [
+      Test<"Or<T,T>", [ExpectTo<Equal<true, Or<true, true>>>]>,
+      Test<"Or<T,F>", [ExpectTo<Equal<true, Or<true, false>>>]>,
+      Test<"Or<F,T>", [ExpectTo<Equal<true, Or<false, true>>>]>,
+      Test<"Or<F,F>", [ExpectTo<Equal<false, Or<false, false>>>]>,
+    ]
+  >;
+  test("Truth Table for A v B", () => {
+    const or: OrPasses = [
+      "Or<A, B>",
+      [
+        ["Or<T,T>", [t]],
+        ["Or<T,F>", [t]],
+        ["Or<F,T>", [t]],
+        ["Or<F,F>", [t]],
+      ],
+    ];
+    expect(or).toBeTruthy();
+  });
+});
