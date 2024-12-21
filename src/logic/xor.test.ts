49111dce285ce1ef75e4b8de26f6cfe490427a9e
Mark Manley <mark.manley3@icloud.com>
2024-12-20 21:25:59 -0500

    create xor w/ Truth Table tests

---
+++ src/logic/xor.test.ts
@@ -0,0 +1,31 @@
+import { expect, suite, test } from "vitest";
+import { ExpectTo } from "../sanity/expect";
+import { Equal } from "../sanity/equal";
+import { Suite } from "../sanity/suite";
+import { Test } from "../sanity/test";
+import { t } from "../sanity/true";
+import { Xor } from "./xor";
+
+suite("Xor<A, B>", () => {
+  type XorPasses = Suite<
+    "Nor<A, B>",
+    [
+      Test<"Xor<T,T>", [ExpectTo<Equal<false, Xor<true, true>>>]>,
+      Test<"Xor<T,F>", [ExpectTo<Equal<true, Xor<true, false>>>]>,
+      Test<"Xor<F,T>", [ExpectTo<Equal<true, Xor<false, true>>>]>,
+      Test<"Xor<F,F>", [ExpectTo<Equal<false, Xor<false, false>>>]>,
+    ]
+  >;
+  test("Truth Table for Xor<A, B>", () => {
+    const xor: XorPasses = [
+      "Nor<A, B>",
+      [
+        ["Xor<T,T>", [t]],
+        ["Xor<T,F>", [t]],
+        ["Xor<F,T>", [t]],
+        ["Xor<F,F>", [t]],
+      ],
+    ];
+    expect(xor).toBeTruthy();
+  });
+});
