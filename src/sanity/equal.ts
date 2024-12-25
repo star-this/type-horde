a4db78095079f23b5f2c5269aa3c9808516ec630
Mark Manley <mark.manley3@icloud.com>
2024-12-24 20:06:48 -0500

    refactor Equal to be more straightforward in explanation

--- src/sanity/equal.ts
+++ src/sanity/equal.ts
@@ -8,7 +8,7 @@ export type Equals<A, B> = Equal<A, B>;
  * ? Expect types X and Y to be equal.
  * ! Equal<A, X & Y> does not work if, e.g.,
  * ! A union (B), but (B) union (X & Y),
- * * @example Equals<true, t>
+ * * @example Equal<true, true>
  */
 export type Equal<A, B> = (<T>() => T extends A ? 1 : 2) extends <
   T,
