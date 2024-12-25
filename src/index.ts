73bff5349c87ecf45783c6ab24659bb4c24c90cd
Mark Manley <mark.manley3@icloud.com>
2024-12-24 20:22:39 -0500

    remove built-in expansion types: move to index.ts with exports

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,27 @@
  */
 
 // built-in-expansions
-export { PrettyReadable, NonNullish, Nullish } from "./built-in-expansions.js";
+/**
+ * ? PrettyReadable         Objects         nested types readable PrettyReadable<Type> -> Type (but not just opaque, shallow Type Names!)
+ * ! NonNullish             Unions          Omit null | undefined NonNullish<Type | undefined> -> Type
+ * * Nullish                Unions          Add null | undefined  Nullish<Type>
+ */
+//
+/**
+ * ? wrap types to make theme more readable on hover / two-slash
+ */
+export type PrettyReadable<T> = {
+  [K in keyof T]: T[K];
+} & {};
+/**
+ * ? Rename of NonNullable<T>
+ */
+export type NonNullish<T> = NonNullable<T>;
+/**
+ * ? Inverse (?) of NonNullish<T>
+ */
+export type Nullish<T> = T | null | undefined;
+
 // sanity
 export { Equal, Equals } from "./sanity/equal.js";
 export {
